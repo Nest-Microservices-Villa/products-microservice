e4652987b8b397ac2647bd0e850f2fca323ba156
Jose Andres Villa Loza <villaa@epsas.com>
2024-12-19 14:04:34 -0400

    Prisam-Sqlite

--- src/config/envs.ts
+++ src/config/envs.ts
@@ -3,11 +3,13 @@ import * as joi from 'joi';
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 interface EnvVars {
   PORT: number;
+  DATABASE_URL: string;
 }
 
 const envsSchema = joi
   .object({
     PORT: joi.number().required(),
+    DATABASE_URL: joi.string().required(),
   })
   .unknown(true);
 const { error, value } = envsSchema.validate(process.env);
@@ -19,4 +21,5 @@ if (error) {
 const envVars: EnvVars = value;
 export const envs = {
   port: envVars.PORT,
+  databaseUrl: envVars.DATABASE_URL,
 };
